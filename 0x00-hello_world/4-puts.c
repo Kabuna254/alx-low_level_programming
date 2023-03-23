f9fd5f4c9067be81703f957fe8d28e56e190d708
Kabuna254 <youngdanny683@gmail.com>
2023-03-23 05:57:55 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
-* main - entry point
-* return - always 0 (success)
+* main - Entry point
+* Description: This program prints a string using put function
+* Return: Always 0 (success)
 */
 int main(void)
 {
